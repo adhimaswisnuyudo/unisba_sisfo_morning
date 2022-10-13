35fb113281b6d01428c27adaa20e6a6ca2bc43d6
adhimaswisnuyudo <adhimas93@ymail.com>
2022-10-13 09:52:50 +0700

    ongoing logind dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -15,12 +15,17 @@ class _LoginPageState extends State<LoginPage> {
         body: ListView(
       children: [
         Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(40),
-            )),
             height: 0.4 * deviceHeight,
-            color: Colors.blue,
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 5)
+                ],
+                color: Colors.blue,
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.elliptical(deviceWidth, 100))),
             child: Column(
               children: [
                 SizedBox(height: deviceHeight * 0.03),
@@ -50,13 +55,57 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
                 SizedBox(height: 5),
-                Text("Universitas Islam Bandung",
+                const Text("Universitas Islam Bandung",
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 20,
                         fontWeight: FontWeight.bold)),
               ],
             )),
+        SizedBox(height: deviceHeight * 0.2),
+        Container(
+          padding: EdgeInsets.only(left: 10),
+          child: const Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(height: deviceHeight * 0.02),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Card(
+            elevation: 5,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border:
+                          Border(bottom: BorderSide(color: Colors.grey[200]!))),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Username",
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Password",
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ],
     ));
   }
